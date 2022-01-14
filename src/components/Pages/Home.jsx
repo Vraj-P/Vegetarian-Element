b2eadf3bf15d120e8bd34f05e733f5dd3ad0e4f5
Khush Patel <k425pate@uwaterloo.ca>
2022-01-14 17:15:57 -0500

    Added footer

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../styles/styles.css';
 import HeroSection from '../landingSection/landingSection';
 import Navbar from '../navbar/navbar';
+import Footer from '../Footer/Footer';
 
 export default function Home() {
     return (
         <>  
             <Navbar/>
             <HeroSection/>
+            <Footer/>
         </>
     );
 }
